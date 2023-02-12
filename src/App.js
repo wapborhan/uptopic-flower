7ea33e114a0190c60fd0393520c1e2cc36b47366
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-02-12 21:40:07 +0600

    Section Add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import Blog from "./components/Blog";
 import Item from "./components/Item";
 import { Footer } from "./components/Footer";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <>
       <Header />
       <main>
+        <Slider />
         <Item />
         <Blog />
       </main>
